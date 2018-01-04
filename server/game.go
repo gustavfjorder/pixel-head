7987a3a9b161813a07a128c8c7fdd29b76a8b10b
Magnus Hansen <magnus.h1993@gmail.com>
2018-01-04 13:41:57 +0100

    Better Player saving in space

--- server/game.go
+++ server/game.go
@@ -4,15 +4,19 @@ import (
 	"github.com/faiface/pixel"
 	. "github.com/pspaces/gospace"
 	"time"
+	"encoding/gob"
 )
 
 func StartGame(players []Player) {
 	spc := NewSpace("tcp://localhost:31415/game1")
 
+	// Register Vec for encoding to space
+	gob.Register(pixel.Vec{})
+
 	// Save players into space
 	for i, player := range players {
 		players[i].Pos = player.Pos.Add(pixel.V(1, 1))
-		spc.Put(player.Id, player.Pos.XY)
+		spc.Put("player", player.Id, player.Pos)
 	}
 
 	// Game loop
@@ -21,12 +25,12 @@ func StartGame(players []Player) {
 		var playerPos pixel.Vec
 
 		// Load players to handle
-		loopPlayers, _ := spc.GetAll(&playerId, &playerPos)
+		loopPlayers, _ := spc.GetAll("player", &playerId, &playerPos)
 
 		for _, tPlayer := range loopPlayers {
-			player := Player{tPlayer.GetFieldAt(0).(string), tPlayer.GetFieldAt(2).(pixel.Vec)}
+			player := Player{tPlayer.GetFieldAt(1).(string), tPlayer.GetFieldAt(2).(pixel.Vec)}
 
-			spc.Put(player.Id, player.Pos.XY)
+			spc.Put("player", player.Id, player.Pos)
 		}
 
 		time.Sleep(time.Second / 60)
