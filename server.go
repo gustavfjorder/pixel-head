08f612b82ff97c5ff166b3ace47c66a799c8cf9c
Magnus Hansen <magnus.h1993@gmail.com>
2018-01-04 21:52:15 +0100

    Server - Simplifying server app

--- server.go
+++ server.go
@@ -2,34 +2,31 @@ package main
 
 import (
 	"fmt"
-	"github.com/faiface/pixel"
 	. "github.com/pspaces/gospace"
 	"github.com/gustavfjorder/pixel-head/server"
-	"github.com/gustavfjorder/pixel-head/server/model"
 )
 
 const N = 1
 
 func main() {
-	spc := NewSpace("tcp://localhost:31414/room1")
-	var id string
-	awaiting := make(map[string]bool, N)
+	lounge := NewSpace("tcp://localhost:31414/lounge")
+
+	awaiting := make([]string, 0, N)
+
 	for {
-		spc.Get("client", &id)
-		fmt.Println("player", id, "connected")
-		if _, ok := awaiting[id]; !ok {
-			awaiting[id] = true
-		}
-		if len(awaiting) >= N {
-			cpy := make([]model.Player, N)
-			i := 0
-			for k, _ := range awaiting {
-				spc.Put(k, "ready")
-				cpy[i] = model.Player{Id: k, Pos: pixel.V(0, 0)}
-				i++
+		var id string
+		lounge.Get("client", &id)
+
+		fmt.Println("Player '" + id + "' has connected")
+
+		awaiting = append(awaiting, id)
+
+		if len(awaiting) == cap(awaiting) {
+			for _, id := range awaiting {
+				lounge.Put(id, "ready")
 			}
-			awaiting = make(map[string]bool, N)
-			go server.StartGame(cpy)
+
+			go server.StartGame(awaiting)
 		}
 	}
 }
