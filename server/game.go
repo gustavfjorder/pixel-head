5cb858ef1eddb1802767a5c85d063144c1a45840
Magnus Hansen <magnus.h1993@gmail.com>
2018-01-04 12:59:38 +0100

    Create game loop

--- server/game.go
+++ server/game.go
@@ -8,11 +8,27 @@ import (
 
 func StartGame(players []Player) {
 	spc := NewSpace("tcp://localhost:31415/game1")
+
+	// Save players into space
+	for i, player := range players {
+		players[i].Pos = player.Pos.Add(pixel.V(1, 1))
+		spc.Put(player.Id, player.Pos.XY)
+	}
+
+	// Game loop
 	for {
-		for i, player := range players {
-			players[i].Pos = player.Pos.Add(pixel.V(1, 1))
+		var playerId string
+		var playerPos pixel.Vec
+
+		// Load players to handle
+		loopPlayers, _ := spc.GetAll(&playerId, &playerPos)
+
+		for _, tPlayer := range loopPlayers {
+			player := Player{tPlayer.GetFieldAt(0).(string), tPlayer.GetFieldAt(2).(pixel.Vec)}
+
 			spc.Put(player.Id, player.Pos.XY)
 		}
+
 		time.Sleep(time.Second / 60)
 	}
 }
