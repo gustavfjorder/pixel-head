f933e57f040dfa8e52ebd372d79fb443bdd073fb
Magnus Hansen <magnus.h1993@gmail.com>
2018-01-05 10:11:15 +0100

    Server - create shoot when requested

--- model/Shoot.go
+++ model/Shoot.go
@@ -9,12 +9,14 @@ type Shoot struct{
 	Start pixel.Vec
 	Angle float64
 	StartTime time.Time
-	Speed float64
+	Weapon Weapon
 }
 
 func (s Shoot) GetPos() (v pixel.Vec) {
 	dt := time.Now().Sub(s.StartTime).Seconds() / 1000
-	return s.Start.Add(pixel.V(s.Speed,0).Scaled(dt).Rotated(s.Angle))
+	newPos := pixel.V(s.Weapon.Speed,0).Scaled(dt).Rotated(s.Angle)
+
+	return s.Start.Add(newPos)
 }
 
 
