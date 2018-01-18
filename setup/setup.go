acaccfa8cacd9c9089fc9ebc4d9cb2907769b9be
Morbi95 <morbitech1@gmail.com>
2018-01-18 21:22:01 +0200

    Fixed ports and send-recv ip

--- setup/setup.go
+++ setup/setup.go
@@ -7,30 +7,29 @@ import (
 )
 
 func RegisterModels() {
-	// Register models for encoding to spc
-	// Register models for encoding to spc
+	 //Register models for encoding to spc
+	 //Register models for encoding to spc
 	gob.Register(model.Request{})
 	gob.Register([]model.Request{})
 	gob.Register(model.Player{})
 	gob.Register([]model.Player{})
 	gob.Register(model.Shot{})
 	gob.Register([]model.Shot{})
+	gob.Register([]model.Barrel{})
+	gob.Register([]model.Lootbox{})
 	gob.Register(model.Map{})
 	gob.Register(model.Wall{})
 	gob.Register(model.Segment{})
 	gob.Register(model.Point{})
 	gob.Register(model.State{})
 	gob.Register(model.Updates{})
+	gob.Register(model.Entity{})
 	gob.Register(&model.Barrel{})
 	gob.Register(&model.WeaponBase{})
 	gob.Register(&model.Shotgun{})
 	gob.Register(&model.Handgun{})
 	gob.Register(&model.Rifle{})
 	gob.Register(&model.Knife{})
-	gob.Register([]model.FastZombie{})
-	gob.Register([]model.SlowZombie{})
-	gob.Register([]model.BombZombie{})
-	gob.Register([]model.Zombie{})
 	gob.Register(&model.FastZombie{})
 	gob.Register(&model.SlowZombie{})
 	gob.Register(&model.BombZombie{})
